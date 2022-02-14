e8bb44adece27ac8d7257a196ed33d66a450f32f
JeenYokesh <jeenyokesh@gmail.com>
2022-02-14 22:53:23 +0530

    Performance Improvement

---
+++ app/src/main/java/com/in10mServiceMan/widgets/CustomRegularEditTextView.java
@@ -0,0 +1,25 @@
+package com.in10mServiceMan.widgets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+import com.in10mServiceMan.R;
+
+public class CustomRegularEditTextView extends AppCompatEditText {
+    public CustomRegularEditTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if (!isInEditMode())
+            applyFont(context);
+    }
+
+    @SuppressLint("NewApi")
+    private void applyFont(Context context) {
+        Typeface typeface = context.getResources().getFont(R.font.unineueregular);
+        setTypeface(typeface);
+    }
+}
