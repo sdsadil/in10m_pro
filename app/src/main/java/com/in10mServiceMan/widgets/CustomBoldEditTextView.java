fc0b1146173102cc091baf35c5b0c74d2f2ad9e2
JeenYokesh <jeenyokesh@gmail.com>
2022-02-14 22:32:08 +0530

    Performance Improvement

---
+++ app/src/main/java/com/in10mServiceMan/widgets/CustomBoldEditTextView.java
@@ -0,0 +1,26 @@
+package com.in10mServiceMan.widgets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.in10mServiceMan.R;
+
+public class CustomBoldEditTextView extends AppCompatEditText {
+    public CustomBoldEditTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if (!isInEditMode())
+            applyFont(context);
+    }
+
+    @SuppressLint("NewApi")
+    private void applyFont(Context context) {
+        Typeface typeface = context.getResources().getFont(R.font.unineuebold);
+        setTypeface(typeface);
+    }
+}
---
+++ app/src/main/java/com/in10mServiceMan/widgets/CustomLightTextView.java
@@ -0,0 +1,25 @@
+package com.in10mServiceMan.widgets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.in10mServiceMan.R;
+
+public class CustomLightTextView extends AppCompatTextView {
+    public CustomLightTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if (!isInEditMode())
+            applyFont(context);
+    }
+
+    @SuppressLint("NewApi")
+    private void applyFont(Context context) {
+        Typeface typeface = context.getResources().getFont(R.font.unineuelight);
+        setTypeface(typeface);
+    }
+}
