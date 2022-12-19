20b71b6909a9a42eb8885ca07555d02a80a472c6
JeenYokesh <jeenyokesh@gmail.com>
2022-12-19 12:32:13 +0530

    Jira ticked Resolved 8.https://gvkw.atlassian.net/browse/IT-41

--- app/src/main/java/com/in10mServiceMan/ui/activities/privacy_policy/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/in10mServiceMan/ui/activities/privacy_policy/PrivacyPolicyActivity.kt
@@ -3,12 +3,18 @@ package com.in10mServiceMan.ui.activities.privacy_policy
 import android.annotation.SuppressLint
 import com.in10mServiceMan.ui.base.In10mBaseActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.in10mServiceMan.R
 import kotlinx.android.synthetic.main.activity_privacy_policy.*
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
+import com.github.barteksc.pdfviewer.util.FitPolicy
 import com.in10mServiceMan.ui.accound_edit.policy_and_terms_api.IPrivacyPolicyView
 import com.in10mServiceMan.ui.accound_edit.policy_and_terms_api.PolicyAndTermsResponse
 import com.in10mServiceMan.ui.accound_edit.policy_and_terms_api.PrivacyPolicyPresenter
@@ -16,28 +22,18 @@ import com.in10mServiceMan.ui.apis.APIClient
 import com.in10mServiceMan.utils.Constants
 import com.in10mServiceMan.utils.SharedPreferencesHelper
 
-class PrivacyPolicyActivity : In10mBaseActivity(), IPrivacyPolicyView {
-    override fun onPrivacyPolicyCompleted(mPost: PolicyAndTermsResponse) {
-        destroyDialog()
-        if (mPost.status == 1) {
-            accept_PP.visibility = View.GONE
-            Constants.GlobalSettings.privacyPolicy = false
-        }
-    }
-
-    override fun onPrivacyPolicyFailed(msg: String) {
-        destroyDialog()
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-    }
+class PrivacyPolicyActivity : In10mBaseActivity(), IPrivacyPolicyView, OnPageChangeListener,
+    OnLoadCompleteListener, OnPageErrorListener {
 
     val privacyPolicyPresenter = PrivacyPolicyPresenter(this)
+    var SAMPLE_FILE = "privacypolicy_pro.pdf"
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_privacy_policy)
         button_close.setOnClickListener {
-            finish()
+            onBackPressed()
         }
         privacyPolicyWV.settings.javaScriptEnabled = true
         privacyPolicyWV.loadUrl("https://docs.google.com/viewer?embedded=true&url=" + APIClient.privacyPolicy)
@@ -54,6 +50,20 @@ class PrivacyPolicyActivity : In10mBaseActivity(), IPrivacyPolicyView {
                     .toInt(), 2
             )
         }
+        displayFromAsset(SAMPLE_FILE)
+    }
+
+    private fun displayFromAsset(assetFileName: String) {
+        pdfView.fromAsset(assetFileName)
+            .defaultPage(0)
+            .onPageChange(this)
+            .enableAnnotationRendering(true)
+            .onLoad(this)
+            .scrollHandle(DefaultScrollHandle(this))
+            .spacing(10) // in dp
+            .onPageError(this)
+            .pageFitPolicy(FitPolicy.BOTH)
+            .load()
     }
 
     inner class myWebViewClient : WebViewClient() {
@@ -61,5 +71,30 @@ class PrivacyPolicyActivity : In10mBaseActivity(), IPrivacyPolicyView {
             view.loadUrl(url)
             return true
         }
+    }
+
+    override fun onPrivacyPolicyCompleted(mPost: PolicyAndTermsResponse) {
+        destroyDialog()
+        if (mPost.status == 1) {
+            accept_PP.visibility = View.GONE
+            Constants.GlobalSettings.privacyPolicy = false
+        }
+    }
+
+    override fun onPrivacyPolicyFailed(msg: String) {
+        destroyDialog()
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPageChanged(page: Int, p1: Int) {
+        Log.e("onPageChanged", "onPageChanged $page")
+    }
+
+    override fun loadComplete(p0: Int) {
+        val meta = pdfView.documentMeta
+    }
+
+    override fun onPageError(p0: Int, t: Throwable?) {
+        t?.printStackTrace()
     }
 }
--- app/src/main/java/com/in10mServiceMan/ui/activities/termsandcondition/TermsAndCondition.kt
+++ app/src/main/java/com/in10mServiceMan/ui/activities/termsandcondition/TermsAndCondition.kt
@@ -1,10 +1,16 @@
 package com.in10mServiceMan.ui.activities.termsandcondition
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle
+import com.github.barteksc.pdfviewer.util.FitPolicy
 import com.in10mServiceMan.R
 import com.in10mServiceMan.ui.accound_edit.policy_and_terms_api.ITermsConditionsView
 import com.in10mServiceMan.ui.accound_edit.policy_and_terms_api.PolicyAndTermsResponse
@@ -13,23 +19,16 @@ import com.in10mServiceMan.ui.apis.APIClient
 import com.in10mServiceMan.ui.base.In10mBaseActivity
 import com.in10mServiceMan.utils.Constants
 import com.in10mServiceMan.utils.SharedPreferencesHelper
+import kotlinx.android.synthetic.main.activity_privacy_policy.*
 import kotlinx.android.synthetic.main.activity_terms_and_condition.*
+import kotlinx.android.synthetic.main.activity_terms_and_condition.button_close
+import kotlinx.android.synthetic.main.activity_terms_and_condition.pdfView
 
-class TermsAndCondition : In10mBaseActivity(), ITermsConditionsView {
-    override fun onTermsConditionsCompleted(mPost: PolicyAndTermsResponse) {
-        destroyDialog()
-        if (mPost.status == 1)  {
-            accept_TC.visibility = View.GONE
-            Constants.GlobalSettings.termsConditions = false
-        }
-    }
+class TermsAndCondition : In10mBaseActivity(), ITermsConditionsView, OnPageChangeListener,
+    OnLoadCompleteListener, OnPageErrorListener {
+    var SAMPLE_FILE = "terms_condition_pro.pdf"
+    val termsConditionsPresenter = TermsConditionsPresenter(this)
 
-    override fun onTermsConditionsFailed(msg: String) {
-        destroyDialog()
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
-    }
-
-    val termsConditionsPresenter =  TermsConditionsPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,8 +47,11 @@ class TermsAndCondition : In10mBaseActivity(), ITermsConditionsView {
         accept_TC.setOnClickListener {
             showProgressDialog("")
             termsConditionsPresenter.termsConditions(
-                SharedPreferencesHelper.getString(this, Constants.SharedPrefs.User.USER_ID, "0")!!.toInt(), 2)
+                SharedPreferencesHelper.getString(this, Constants.SharedPrefs.User.USER_ID, "0")!!
+                    .toInt(), 2)
         }
+
+        displayFromAsset(SAMPLE_FILE)
     }
 
     inner class myWebViewClient : WebViewClient() {
@@ -57,5 +59,43 @@ class TermsAndCondition : In10mBaseActivity(), ITermsConditionsView {
             view.loadUrl(url)
             return true
         }
+    }
+
+    override fun onTermsConditionsCompleted(mPost: PolicyAndTermsResponse) {
+        destroyDialog()
+        if (mPost.status == 1) {
+            accept_TC.visibility = View.GONE
+            Constants.GlobalSettings.termsConditions = false
+        }
+    }
+
+    override fun onTermsConditionsFailed(msg: String) {
+        destroyDialog()
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+
+    private fun displayFromAsset(assetFileName: String) {
+        pdfView.fromAsset(assetFileName)
+            .defaultPage(0)
+            .onPageChange(this)
+            .enableAnnotationRendering(true)
+            .onLoad(this)
+            .scrollHandle(DefaultScrollHandle(this))
+            .spacing(10) // in dp
+            .onPageError(this)
+            .pageFitPolicy(FitPolicy.BOTH)
+            .load()
+    }
+
+    override fun onPageChanged(page: Int, p1: Int) {
+        Log.e("onPageChanged", "onPageChanged $page")
+    }
+
+    override fun loadComplete(p0: Int) {
+        val meta = pdfView.documentMeta
+    }
+
+    override fun onPageError(p0: Int, t: Throwable?) {
+        t?.printStackTrace()
     }
 }
